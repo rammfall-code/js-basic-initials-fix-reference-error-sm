0a37320742b66dcd912f6aef311f44cd31e06782
Anya <81263768+anyabuu@users.noreply.github.com>
2025-03-15 21:23:23 +0200

    info user made

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-const name = prompt('Enter your name');
+function askInfo(message) {
+  let name;
 
-alert(`Hi, ${name}. Your age is ${age}`);
+  do {
+    name = prompt(message);
+
+    if (name === null) {
+      return null;
+    }
+  } while (name.trim() === '' || /\d/.test(name));
+
+  return name;
+}
+
+function askInfoNumber(message) {
+  let number;
+
+  do {
+    number = prompt(message);
+
+    if (number === null) {
+      return null;
+    }
+  } while (isNaN(number) || number.trim() === '');
+
+  return +number;
+}
+
+const name = askInfo('Enter your name');
+
+if (name === null) {
+  alert('you cancelled');
+} else {
+  const age = askInfoNumber('Enter your age');
+  if (age === null) {
+    alert('you cancelled');
+  } else {
+    alert(`Hi, ${name}. Your age is ${age}`);
+  }
+}
